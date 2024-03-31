5c089b7d3d2c7dcca067c69e4165a00fd48bca06
Tim Peoples <tim@toolman.org>
2024-03-31 13:19:00 -0700

    Replace 'New' with Context-specific constructors
    1. Added constructor funcs 'WithCancel', 'WithDeadline', and
       'WithTimeout' that wrap their similarly named funcs from the
       'context' package.
    
    2. The constructor func 'New' is now deprecated; the new 'WithCancel'
       func does the same thing.
    
    3. Renamed 'GoFunc' to 'ContextFunc'
    
    4. Updated package docs accordingly.

--- pkg/errgroupx/errgroupx.go
+++ pkg/errgroupx/errgroupx.go
@@ -1,46 +1,47 @@
 // Copyright © 2024 Timothy E. Peoples
 
 // Package errgroupx provides an opinionated convenience wrapper around
-// package golang.org/x/sync/errgroup that makes it easier to deal with
-// Groups and Contexts in a consistent manner.
+// package golang.org/x/sync/errgroup that provides a consistent manner
+// of dealing with Contexts and (error) Groups.
 //
-// A typical usecase would be:
+// A typical usecase might be:
 //
-//	// run executes the receiver's doTHIS and doTHAT methods
-//	// concurrently until both succeed or one fails.
+//	// The run methods executes its receiver's doTHIS and doTHAT methods
+//	// concurrently until either both succeed or one of them fails.
 //	func (t *thing) run(ctx context.Context) error {
-//		eg, ctx, cancel := errgroupx.New(ctx)
+//		eg, ctx, cancel := errgroupx.WithCancel(ctx)
 //		defer cancel()
 //
-//		for _, gf := range[]errgroupx.GoFunc{t.doTHIS, t.doTHAT} {
-//			eg.GoContext(ctx, gf)
-//		}
+//		eg.GoContext(ctx, t.doTHIS)
+//		eg.GoContext(ctx, t.doTHAT)
 //
 //		return eg.Wait()
 //	}
 //
 //	func (t *thing) doTHIS(ctx context.Context) error {
-//		//...do THIS with t and ctx returning an error if need be.
+//		//...do THIS with t and ctx returning an error as appropriate.
 //	}
 //
 //	func (t *thing) doTHAT(ctx context.Context) error {
-//		//...do THAT with t and ctx returning an error if need be.
+//		//...do THAT with t and ctx returning an error as appropriate.
 //	}
 //
-// Note that if you do not use the added constructor function (`New`) or
-// the Context-aware methods, this package is a drop-in replacement for
-// golang.org/x/sync/errgroup.
+// Note that if you make no use of the added constructor functions
+// ('WithCancel', 'WithDeadline' or 'WithTimeout') or the Context aware
+// methods ('GoContext' or 'TryGoContext'), then this package can be
+// used as a drop-in replacement for golang.org/x/sync/errgroup.
 package errgroupx
 
 import (
 	"context"
+	"time"
 
 	"golang.org/x/sync/errgroup"
 )
 
 type (
 	// Group is a convenience wrapper around an embedded Group from package
-	// golang.org/x/sync/errgroup that adds two new methods: GoContext and
+	// golang.org/x/sync/errgroup which adds two new methods: GoContext and
 	// TryGoContext. All of the other methods on the embedded Group type are
 	// available here as well.
 	Group struct {
@@ -50,34 +51,62 @@ type (
 	group = errgroup.Group
 )
 
-// New is a simple wrapper around context.WithCancel and errgroup.WithContext
-// returning a new Group, a derived Context, and a CancelFunc. The derived
-// Context is canceled the first time a function passed to GoContext returns
-// a non-nil error or the first time Wait returns, whichever occurs first.
+// New is equivalent to WithCancel.
+//
+// Deprecated: use WithCancel instead.
 func New(ctx context.Context) (*Group, context.Context, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(ctx)
+	return newGroup(context.WithCancel(ctx))
+}
+
+// WithCancel is a wrapper around errgroup.WithContext and context.WithCancel
+// returning a new Group, a derived Context, and a CancelFunc. The derived
+// Context is canceled the first time a function passed to GoContext (or
+// similar) returns a non-nil error, or the first time Wait returns, whichever
+// occurs first.
+//
+// See package 'context' about what to do with the CancelFunc.
+func WithCancel(ctx context.Context) (*Group, context.Context, context.CancelFunc) {
+	return newGroup(context.WithCancel(ctx))
+}
+
+// WithDeadline is a similar to WithCancel but wraps context.WithDeadline
+// instead of context.WithCancel.
+func WithDeadline(ctx context.Context, d time.Time) (*Group, context.Context, context.CancelFunc) {
+	return newGroup(context.WithDeadline(ctx, d))
+}
+
+// WithTimeout is a similar to WithCancel but wraps context.WithTimeout
+// instead of context.WithCancel.
+func WithTimeout(ctx context.Context, timeout time.Duration) (*Group, context.Context, context.CancelFunc) {
+	return newGroup(context.WithTimeout(ctx, timeout))
+}
+
+// newGroup provides common logic for the constructor functions WithCancel,
+// WithDeadline, and WithTimeout.
+func newGroup(ctx context.Context, cancel context.CancelFunc) (*Group, context.Context, context.CancelFunc) {
 	group, ctx := errgroup.WithContext(ctx)
 	return &Group{group}, ctx, cancel
 }
 
-// GoFunc is the function type passed to GoContext or TryGoContext.
-type GoFunc func(context.Context) error
+// ContextFunc is the function type passed to GoContext or TryGoContext.
+type ContextFunc func(context.Context) error
 
-func (gf GoFunc) egFunc(ctx context.Context) func() error {
-	return func() error { return gf(ctx) }
+func (cf ContextFunc) do(ctx context.Context) func() error {
+	return func() error { return cf(ctx) }
 }
 
-// GoContext is a wrapper around the Go method from golang.org/x/sync/errgroup
-// that accepts an anonymous function with a Context parameter. The Context
-// provided here is passed to the GoFunc unchanged.
-func (g *Group) GoContext(ctx context.Context, f GoFunc) {
-	g.group.Go(f.egFunc(ctx))
+// GoContext is a wrapper around the (*Group).Go method from package
+// golang.org/x/sync/errgroup that accepts an anonymous function with
+// a Context parameter. The Context provided here is passed to the
+// ContextFunc unchanged.
+func (g *Group) GoContext(ctx context.Context, cfunc ContextFunc) {
+	g.group.Go(cfunc.do(ctx))
 }
 
-// TryGoContext is a wrapper around the similarly named TryGo method from
+// TryGoContext is a wrapper around the (*Group).TryGo method from package
 // golang.org/x/sync/errgroup that accepts an anonymous function with a
-// Context parameter. The Context provided here is passed to the GoFunc
+// Context parameter. The Context provided here is passed to the ContextFunc
 // unchanged.
-func (g *Group) TryGoContext(ctx context.Context, f GoFunc) bool {
-	return g.group.TryGo(f.egFunc(ctx))
+func (g *Group) TryGoContext(ctx context.Context, cfunc ContextFunc) bool {
+	return g.group.TryGo(cfunc.do(ctx))
 }
